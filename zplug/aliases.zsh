9c270f0d007ca0d7dce24d150c6aecbdf209182e
Jeroen van Erp <jeroen@hierynomus.com>
2017-01-11 21:19:34 +0100

    Updated config

--- zplug/aliases.zsh
+++ zplug/aliases.zsh
@@ -3,3 +3,6 @@ alias gitrm="git ls-files -d | xargs git rm"
 alias java6="export JAVA_HOME=$(/usr/libexec/java_home -v 1.6)"
 alias java7="export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)"
 alias java8="export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)"
+
+alias docker_rm="docker rm -v $(docker ps -a -q -f status=exited)"
+alias docker_rmi="docker rmi $(docker images -f "dangling=true" -q)"
