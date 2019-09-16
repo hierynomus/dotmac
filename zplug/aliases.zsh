74edd5447258cc5e7466887f272ac94838893fcf
Jeroen van Erp <jeroen@hierynomus.com>
2019-09-16 09:36:35 +0200

    Migrate to new laptop

--- zplug/aliases.zsh
+++ zplug/aliases.zsh
@@ -4,8 +4,9 @@ alias java6="export JAVA_HOME=$(/usr/libexec/java_home -v 1.6)"
 alias java7="export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)"
 alias java8="export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)"
 
-alias docker_rm="docker rm -v $(docker ps -a -q -f status=exited)"
-alias docker_rmi="docker rmi $(docker images -f "dangling=true" -q)"
+# alias docker_rm="docker rm -v $(docker ps -a -q -f status=exited)"
+# alias docker_rmi="docker rmi $(docker images -f "dangling=true" -q)"
 
+alias ls='ls -G'
 unalias gs
 alias gs='git status'
--- zplug/init.zsh
+++ zplug/init.zsh
@@ -1,5 +1,4 @@
 #!/bin/zsh
-
 local DIR=$(dirname $0)
 load_functions() {
   local function_glob='^([_.]*|prompt_*_setup|README*)(-.N:t)'
@@ -29,3 +28,23 @@ source ${DIR}/config.zsh
 if [[ -f "${HOME}/.secrets.zsh" ]]; then
   source "${HOME}/.secrets.zsh"
 fi
+
+
+# zsh-pipenv-shell-activate() {
+#   if [ ! "$(command -v pipenv)" ]; then
+#     echo "Install http://docs.pipenv.org/en/latest/ to use this plugin." > /dev/stderr
+#     return 1
+#   fi
+
+#   if [ -e "$(pwd)/Pipfile" ]; then
+#     if [ ! "$PIPENV_ACTIVE" ]; then
+#       pipenv shell
+#     fi
+#   fi
+# }
+
+# # Add activate to change pwd functions
+# chpwd_functions+=(zsh-pipenv-shell-activate)
+
+# enable pipenv tab completion
+eval "$(pipenv --completion)"
